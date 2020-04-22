9d39e2a13da4b361acfeb00551890c940884ad30
Aman208 <amankumar20804@gmail.com>
2020-04-22 17:37:29 +0530

    Grid View of Categories done

---
+++ lib/category_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
+class CategoryList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+         appBar: AppBar(
+        title: Text("DeliMeals"),
+      ),
+          body: Container(
+         padding: EdgeInsets.all(20),
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                maxCrossAxisExtent: 200,
+              ),
+              itemBuilder: ( ctx , index){
+                return CategoryItem(DUMMY_CATEGORIES[index].title ,DUMMY_CATEGORIES[index].color );
+              },
+              itemCount: DUMMY_CATEGORIES.length,)),
+    );
+  }
+}
